c717681d7db48baadb02c2814e34c748e44779a0
Jiří Novotný <409963@muni.cz>
2020-09-15 12:52:18 +0200

    WIP advanced annotation
    - main script for advanced annotation added

--- xmsannotator/R/advanced_annotation.R
+++ xmsannotator/R/advanced_annotation.R
@@ -1,165 +1,220 @@
-advanced_annotation <- function(peak_intensity_table, metabolites, workers) {
-  WGCNA::allowWGCNAThreads(workers)
+# peak_intensity_table
+#   mz
+#   rt
+#   intensity_<file>
+# -----
+#   module
+#   rt_cluster
+#   is_cluster
+#   mz_defect
+#   multiple_match
+#   score
 
-  peak_intensity_table <- as_tibble(peak_intensity_table) %>%
-    distinct(peak_intensity_table, mz, rt, .keep_all = TRUE)
-  metabolites <- dplyr::distinct(metabolites)
+# metabolites
+#   mz
+#   chemical_id
+#   name
+#   formula
+#   monoisotopic_mass
+#   adduct
+#   adduct_mass
+
+# adduct_weights
+#   adduct
+#   adduct_weight
+
+
+nest_clusters <- function(...) {
+  factor(paste(..., sep = "_"))
 }
 
-compute_score <- function(df, adduct_weights) {
-  # TODO line 11-16
+cluster_by_density <- function(x, tolerance) {
+  # FIXME: Though this implementation is same as the original implementation of
+  # group_by_histv1, I dont thing that this clustering algorithm is correct or
+  # even suitable for the clustering data (mainly retention time data).
+  # For the future, consider something as dbscan which is also a density based
+  # clustering algorithm.
+  # Note, that the original condition `... && abs(h$mid - minimum) > tolerance`
+  # smells bad.
 
-  isotopes <- Rdisop::getMolecule(formula)$isotopes[[1]]
-  abundance_ratio <- isotopes[2, isotopes[2,] >= 0.001]
+  bandwith <- try(min(bw.nrd(x), tolerance))
+  if (inherits(bandwith, "try-error"))
+    bandwith <- tolerance
 
-  # TODO: line 26-41 remove water adduct
-  # TODO: line 47-160
-  # TODO: ..186 group by rt with isotopes
+  bins <- seq(min(x) - tolerance, max(x) + tolerance, bandwith)
+  histogram <- hist(x, breaks = bins, plot = FALSE)
 
-  dt %>%
-    add_count(rt_group, sort = TRUE)
-}
+  counter <- 1
+  labels <- integer(length(histogram$density))
+  edges <- histogram$density == 0 & abs(histogram$mids - min(x)) > tolerance
 
-compute_scores <- function(df, adduct_weights) {
-  df %>%
-    group_by(id) %>%
-    mutate(score = { }) %>%
-    ungroup(id)
-}
+  for (i in seq_along(labels)) {
+    if (edges[i])
+      counter <- counter + 1
+    labels[i] <- if (histogram$density[i] > 0) counter else 0
+  }
 
-.pathway_evaluation <- function(dt, metabolites, adduct_weights, score_threshold) {
-  key <- function(x) gsub(pattern = "_.*", replacement = "", x)
+  selection <- histogram$density > 0
+  labels <- labels[selection]
+  mids <- histogram$mids[selection]
+  mids[1] <- min(x)
 
-  dt <- dt %>%
-    mutate(adduct_key = key(adduct), formula_key = key(formula)) %>%
-    inner_join(metabolites, by = c("adduct_key", "formula_key"))
-
-  hmdb_bad <- c("HMDB29244", "HMDB29245", "HMDB29246")
-  dt <- filter(dt, !(chemical_id %in% hmdb_bad))
-
-  # TODO: extract `pathways` from metabolite DB
-  semi_join(metabolites, dt, by = pathway)
-
-  filter(dt, score >= score_threshold)
-}
-
-print_confidence_distribution <- function(df) {
-  unique_chemical_confidence <- df %>%
-    select(chemical_id, confidence) %>%
-    distinct(chemical_id, .keep_all = TRUE) %>%
-    pull(confidence)
-  unique_formula_confidence <- df %>%
-    select(formula, confidence) %>%
-    distinct(formula, .keep_all = TRUE) %>%
-    pull(confidence)
-
-  print("Stage 4 confidence level distribution for unique chemical/metabolite IDs")
-  print(table(unique_chemical_confidence))
-
-  print("Stage 4 confidence level distribution for unique chemical/metabolite formulas")
-  print(table(unique_formula_confidence))
-
-  invisible(df)
-}
-
-step5 <- function(result, adduct_weights, score_threshold = 0) {
-  mz_frequency <- table(result$mz)
-  nonunique_mzs <- names(mz_frequency)[mz_frequency > 1]
-
-  bad_indices <- purrr::map_int(nonunique_mzs, function(value) {
-    subset <- result[result$mz %in% value,]
-
-    good_adduct <- subset$adduct %in% adduct_weights$adduct
-    subset$score[good_adduct] <- subset$score[good_adduct] * 100
-
-    indices <- which(result$mz %in% value)
-    indices <- indices[subset$score != max(subset$score, na.rm = TRUE)]
-
-    for (chemical_id in subset$chemical_id[indices]) {
-      ind <- which(result$chemical_id %in% chemical_id)
-      result$score[ind] <- (length(ind) - 1) * result$score[ind[1]] / length(ind)
-    }
-
-    indices
+  sapply(x, function(value) {
+    labels[which.min(abs(mids - value))]
   })
-
-  result <- result %>%
-    slice(-bad_indices) %>%
-    filter(score >= score_threshold) %>%
-    mutate(multiple_match = is_nonuinque_mz(mz))
-
-
-  print("Stage 4 confidence level distribution for unique chemical/metabolite IDs")
-  # TODO: print(table(unique_chemical_confidence))
-
-  print("Stage 4 confidence level distribution for unique chemical/metabolite formulas")
-  # TODO: print(table(unique_formula_confidence))
-
-  result
 }
 
-compute_multiple_matches <- function (df) {
-  mutate(df, multiple_match = is_nonuinque_mz(mz))
+peak_intensity_clustering <- function(peaks, peak_intensity_matrix, peak_correlation_matrix, correlation_threshold, deep_split, min_cluster_size, network_type) {
+  power <- try(WGCNA::pickSoftThreshold(peak_intensity_matrix)$powerEstimate)
+  if (is.na(power) || inherits(power, "try-error")) {
+    power <- 6
+  }
+
+  dendrogram <- flashClust::flashClust(as.dist(1 - peak_correlation_matrix))
+  dendrogram <- dynamicTreeCut::cutreeDynamic(
+    dendro = dendrogram,
+    distM = 1 - peak_correlation_matrix,
+    minClusterSize = min_cluster_size,
+    pamRespectsDendro = FALSE
+  )
+
+  modules <- try(WGCNA::blockwiseModules(
+      datExpr = peak_intensity_matrix,
+      checkMissingData = FALSE,
+      blocks = dendrogram,
+      blockSizePenaltyPower = 100,
+      power = power,
+      deepSplit = deep_split,
+      detectCutHeight = NULL,
+      minModuleSize = min_cluster_size,
+      pamRespectsDendro = FALSE,
+      mergeCutHeight = 1 - correlation_threshold
+    )$colors)
+
+  if (inherits(modules, "try-error")) {
+    adjacency_matrix <- WGCNA::adjacency(
+      datExpr = peak_intensity_matrix,
+      type = network_type,
+      power = power,
+      corOptions = list(use = "p", method = "pearson")
+    )
+    distance_matrix <- WGCNA::TOMdist(adjacency_matrix)
+
+    dendrogram <- flashClust::flashClust(
+      as.dist(distance_matrix)
+    )
+    dendrogram <- dynamicTreeCut::cutreeDynamic(
+      dendro = dendrogram,
+      distM = distance_matrix,
+      deepSplit = deep_split,
+      minClusterSize = min_cluster_size,
+      pamRespectsDendro = FALSE
+    )
+    modules <- WGCNA::mergeCloseModules(
+      exprData = peak_intensity_matrix,
+      colors = dendrogram,
+      cutHeight = 1 - correlation_threshold,
+      trapErrors = TRUE
+    )$colors
+  }
+
+  tibble::add_column(peaks, peak_cluster = modules)
+}
+
+retention_time_clustering <- function(df, rt_tolerance) {
+  as_tibble(df) %>%
+    group_by(peak_cluster) %>%
+    mutate(
+      rt_cluster = cluster_by_density(mz, {{ rt_tolerance }}),
+      rt_cluster = nest_clusters(peak_cluster, rt_cluster)
+    ) %>%
+    ungroup(peak_cluster)
+}
+
+mass_defect_clustering <- function(df, precision) {
+  mutate(df,
+    mass_defect = cut(mz %% 1, seq(0, 1, {{ precision }}), labels = FALSE),
+    mass_defect_cluster = nest_clusters(rt_cluster, mass_defect),
+  )
 }
 
 compute_delta_mz <- function(df) {
   # NOTE: I changed the computation of delta_ppm becaouse I think the previos
   # version behaved incorectly (see lines 130-140 in multilevelannotationstep4).
-  mutate(df, delta_ppm = round(10^6 * abs(theoretical_mz - mz) / theoretical_mz, 2))
+  mutate(df, delta_ppm = round(10^6 * abs(expected_mass - mz) / expected_mass, 2))
 }
 
-boost_scores <- function(df, boost, mz_diff, rt_diff) {
-  df <- rename_with(df, tolower)
+#compute_scores <- function(df, corelation_threshold, correlation_matrix, adduct_weights, expected_adducts, max_isp, rt_tolerance) {
+#  data.frame(
+#    mz = mz,
+#    time = rt,
+#    MatchCategory = ifelse(multiple_match, "Multiple", "Unique"),
+#    theoretical.mz =,
+#    chemical_ID = chemical_id,
+#    Name = name,
+#    Formula = formula,
+#    MonoisotopicMass = monoisotopic_mass,
+#    Adduct = adduct,
+#    ISgroup =,
+#    Module_RTclust =,
+#    time.y =,
+#    mean_int_vec =,
+#    MD =
+#  )
+#
+#  as_tibble(df) %>%
+#    group_by(chemical_id) %>%
+#    mutate(score = 0) %>%
+#    ungroup()
+#}
 
-  match_peak_by_id <- function(id) id %in% boost$id
-  match_peak_by_mz <- function(mz) abs(mz - boost$mz) < mz * mz_diff
-  match_peak_by_rt <- function(rt) abs(rt - boost$rt) < rt_diff
-
-  boosted <- if (all(c("id", "mz", "rt") %in% colnames(boost))) {
-    match_peak_by_id(id, boost$id)
-  } else if ("id" %in% colnames(boost)) {
-    mapply(function(id, mz, rt) {
-      any(match_peak_by_id(id) & match_peak_by_mz(mz) & match_peak_by_rt(rt))
-    }, id = df$id, mz = df$mz, rt = df$rt)
-  } else {
-    stop("Boost table must atleast contain one column called ID")
-  }
-
-  mutate(df,
-    score = ifelse(boosted, 100 * score, score),
-    confidence = ifelse(boosted, 4, confidence)
-  )
+compute_scores <- function(annotation, adduct_weights) {
+  annotation <- as_tibble(annotation) %>%
+    filter(is_valid_adduct(adduct, formula)) %>%
+    left_join(adduct_weights, by = "adduct")
+  mutate(annotation, score = 0)
 }
 
-step4 <- function(df, adduct_table, adduct_weights, filter_by, boost, mz_diff, rt_diff) {
-  df %>%
-    compute_confidence_scores(
-      adduct_table = adduct_table,
-      adduct_weights = adduct_weights,
-      filter_by = filter_by
+advanced_annotation <- function(peaks, metabolites, adducts, mz_tolerance_ppm, rt_tolerance, correlation_threshold, deep_split, min_cluster_size, network_type, adduct_weights, boost_metabolites, expected_adducts, workers = parallel::detectCores()) {
+  WGCNA::allowWGCNAThreads(workers)
+
+  peaks <- distinct(peaks, mz, rt, .keep_all = TRUE)
+  metabolites <- distinct(metabolites)
+
+  peak_intensity_matrix <- t(select(peaks, starts_with("intensity")))
+  peak_correlation_matrix <- WGCNA::cor(peak_intensity_matrix, use = "p", method = "p")
+
+  peaks %>%
+    peak_intensity_clustering(
+      peak_intensity_matrix = peak_intensity_matrix,
+      peak_correlation_matrix = peak_correlation_matrix,
+      correlation_threshold = correlation_threshold,
+      min_cluster_size = min_cluster_size,
+      network_type = network_type,
+      deep_split = deep_split
     ) %>%
-    boost_scores(
-      boost = boost,
-      mz_diff = mz_diff,
-      rt_diff = rt_diff
+    retention_time_clustering(rt_tolerance = rt_tolerance) %>%
+    mass_defect_clustering(precision = 0.01) %>%
+    xmsannotator::simple_annotation(
+      adducts = adducts,
+      metabolites = metabolites,
+      mz_tolerance_ppm = mz_tolerance_ppm
     ) %>%
-    compute_multiple_matches() %>%
-    print_confidence_distribution()
-}
-
-compute_confidence_scores <- function (df, adduct_table, adduct_weights, filter_by) {
-  monoisotopic <- list(
-    adduct = "M",
-    molecules = 1,
-    adduct_charge = 1,
-    adduct_mass = 0,
-    adduct_mode = NA,
-    adduct_type ="S"
-  )
-  adduct_table <- bind_rows(adduct_table, monoisotopic)
-
-  df %>%
-    group_by(chemical_id) %>%
-    mutate(confidence = 0) %>%
-    ungroup()
+    compute_scores(adduct_weights = adduct_weights) %>%
+    #compute_scores(
+    #  correlation_threshold = correlation_threshold,
+    #  correlation_matrix = correlation_matrix,
+    #  adduct_weights = adduct_weights,
+    #  expected_adducts = as.character(expected_adducts),
+    #  max_isp = as.integer(max_isp),
+    #  rt_tolerance = rt_tolerance,
+    #)
+    evaluate_pathways(pathways = , score_threshold = 0.1) %>%
+    step4(
+      expected_adducts = expected_adducts,
+      boost_metabolites = boost_metabolites,
+      mz_tolerance = 10^6 * mz_tolerance_ppm,
+      rt_tolerance = rt_tolerance
+    ) %>%
+    redundancy_filtering(score_threshold = 0)
 }
