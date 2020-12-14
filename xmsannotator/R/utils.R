aa6410ae483b870f0d97626bea555bc4916ad292
Jiří Novotný <409963@muni.cz>
2020-12-14 03:01:11 +0100

    Enable parquet support

--- xmsannotator/R/utils.R
+++ xmsannotator/R/utils.R
@@ -1,35 +1,83 @@
-validate_table <- function(table, primary_key = NULL, expected_columns = NULL, optional_columns = NULL) {
-  expected_columns <- union(primary_key, expected_columns)
-
-  if (!all(expected_columns %in% colnames(table)))
-    stop("The table must containt these columns: ", toString(expected_columns))
-
-  table <- table %>%
-    dplyr::select(all_of(expected_columns), any_of(optional_columns)) %>%
-    dplyr::distinct()
-
-  if (!is.null(primary_key) && anyDuplicated(dplyr::select(table, primary_key)))
-    stop("Duplicate values in primary key detected: ", toString(primary_key))
-  table
+lexicographic_rank <- function (...) {
+  .o <- order(...)
+  .x <- cbind(...)[.o, ]
+  cumsum(!duplicated(.x))[order(.o)]
 }
 
-load_hdf <- function (path, key) {
-  if (!is.character(path) || !file.exists(path) || !hdf5r::is_hdf5(path))
-    stop("File \"", path, "\" is not a valid HDF5 file.")
-
-  file <- hdf5r::H5File$new(filename = path, mode = "r")
-  on.exit(file$close_all())
-
-  if (!key %in% names(file))
-    stop("File \"", path, "\" does not contains required key \"", key, "\".")
-
-  tibble::as_tibble(file[[key]]$read())
+save_parquet <- function(data, file) {
+  invisible(arrow::write_parquet(data, file))
 }
 
-save_hdf <- function(path, key, data) {
-  file <- hdf5r::H5File$new(filename = path, mode = "w")
-  on.exit(file$close_all())
+load_parquet <- function(file, required_columns, optional_columns = NULL) {
+  df <- tryCatch(
+    arrow::read_parquet(file, col_select = dplyr::any_of(union(required_columns, optional_columns))),
+    error = function(e) stop("The file ", toString(file), " is not a valid Parquet file")
+  )
 
-  file[[as.character(key)]] <- data
-  invisible(data)
+  if (!all(required_columns %in% colnames(df))) {
+    stop("The file ", toString(file), " does not contain any of these required columns: ", toString(required_columns))
+  }
+
+  return(df)
+}
+
+load_csv <- function (file, required_columns, optional_columns = NULL) {
+  df <- readr::read_csv(file)
+  df <- dplyr::select(df, dplyr::any_of(union(required_columns, optional_columns)))
+
+  if (!all(required_columns %in% colnames(df))) {
+    stop("The file ", toString(file), " does not contain any of these required columns: ", toString(required_columns))
+  }
+
+  return(df)
+}
+
+load_peak_table_parquet <- function(file) {
+  load_parquet(file, required_columns = c("peak", "mz", "rt"))
+}
+
+load_adduct_table_parquet <- function(file) {
+  load_parquet(file, required_columns = c("adduct", "charge", "mass", "n_molecules"))
+}
+
+load_compound_table_parquet <- function(file) {
+  compound_table <- load_parquet(
+    file,
+    required_columns = c("monoisotopic_mass", "molecular_formula"),
+    optional_columns = c("compound", "recetox_cid")
+  )
+
+  if (!any(c("compound", "recetox_cid") %in% colnames(compound_table))) {
+    stop("The supplied compound table needs to contain either compound or recetox_cid column")
+  }
+
+  if ("recetox_cid" %in% colnames(compound_table)) {
+    compound_table <- dplyr::mutate(compound_table, compound = recetox_cid)
+  }
+
+  return(compound_table)
+}
+
+load_peak_table_hdf <- function(file) {
+  peak_table <- rhdf5::h5read(file, "peaks")
+  peak_table <- dplyr::select(
+    peak_table,
+    dplyr::any_of("peak"),
+    dplyr::all_of(c("mz", "rt")),
+    dplyr::starts_with("intensity")
+  )
+
+  if (!"peak" %in% colnames(peak_table)) {
+    peak_table <- dplyr::mutate(peak_table, peak = lexicographic_rank(mz, rt))
+  }
+
+  return(peak_table)
+}
+
+load_expected_adducts_csv <- function (file) {
+  load_csv(file, required_columns = "adduct")
+}
+
+load_boost_compounds_csv <- function (file) {
+  load_csv(file, required_columns = "compound", optional_columns = c("mz", "rt"))
 }
