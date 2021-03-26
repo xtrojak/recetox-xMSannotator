72b70df2875fdf7b53a683fa638803b52e3d2bbf
Jiří Novotný <409963@mail.muni.cz>
2021-03-26 12:40:28 +0100

    Utils for loading and checking the proper structure of data

--- xmsannotator/R/utils.R
+++ xmsannotator/R/utils.R
@@ -1,83 +1,124 @@
-lexicographic_rank <- function (...) {
+lexicographic_rank <- function(...) {
   .o <- order(...)
-  .x <- cbind(...)[.o, ]
+  .x <- cbind(...)[.o,]
   cumsum(!duplicated(.x))[order(.o)]
 }
 
+#' @import dplyr
+as_peak_table <- function(data, intensities = FALSE) {
+  optional <- rlang::quo(any_of('peak'))
+  required <- rlang::quo(any_of(c('mz', 'rt')))
+  additional <- rlang::quo(starts_with('intensity'))
+  data <- select(data, !!optional, !!required, if (intensities) !!additional else NULL)
+
+  if (!is.element('peak', colnames(data))) {
+    data$peak <- lexicographic_rank(data$mz, data$rt)
+  }
+
+  stopifnot(anyDuplicated(data$peak) == 0)
+  stopifnot(is.integer(data$peak))
+  stopifnot(is.numeric(data$mz))
+  stopifnot(is.numeric(data$rt))
+
+  return(data)
+}
+
+#' @import dplyr
+as_adduct_table <- function(data) {
+  data <- select(data, 'adduct', 'charge', 'factor', 'mass')
+
+  stopifnot(anyDuplicated(data$adduct) == 0)
+  stopifnot(is.integer(data$charge))
+  stopifnot(is.integer(data$factor))
+  stopifnot(is.numeric(data$mass))
+
+  return(data)
+}
+
+#' @import dplyr
+as_compound_table <- function(data) {
+  optional <- c('compound', 'recetox_cid')
+  required <- c('monoisotopic_mass', 'molecular_formula')
+  data <- select(data, any_of(optional), all_of(required))
+
+  if ('recetox_cid' %in% colnames(data)) {
+    data <- mutate(data, compound = .data$recetox_cid)
+  }
+
+  stopifnot('compound' %in% colnames(data))
+  stopifnot(anyDuplicated(data$compound) == 0)
+  stopifnot(is.numeric(data$monoisotopic_mass))
+  stopifnot(is.character(data$molecular_formula))
+
+  return(data)
+}
+
+#' @import dplyr
+as_ecpected_adducts_table <- function (data) {
+  data <- select(data, 'adduct')
+  return(data)
+}
+
+#' @import dplyr
+as_boosted_compounds_table <- function (data) {
+  data <- select(data, all_of('compound'), any_of(c('mz', 'rt')))
+
+  stopifnot(is.numeric(data$mz))
+  stopifnot(is.numeric(data$rt))
+
+  return(data)
+}
+
+#' @import dplyr
+load_parquet <- function (file, columns) {
+  rlang::with_handlers(
+    arrow::read_parquet(file, col_select = any_of(columns)),
+    error = ~ rlang::abort(paste("The file", toString(file), "seams not to be a valid Parquet file."), parent = .)
+  )
+}
+
+load_csv <- function (file, columns) {
+  data <- readr::read_csv(file)
+  data <- select(data, any_of(columns))
+}
+
+#' @export
+load_peak_table_parquet <- function(file) {
+  data <- load_parquet(file, columns = c("peak", "mz", "rt"))
+  as_peak_table(data)
+}
+
+#' @export
+load_adduct_table_parquet <- function(file) {
+  data <- load_parquet(file, columns = c("adduct", "charge", "mass", "n_molecules"))
+  as_adduct_table(data)
+}
+
+#' @export
+load_compound_table_parquet <- function(file) {
+  data <- load_parquet(file, columns = c("compound", "recetox_cid", "monoisotopic_mass", "molecular_formula"))
+  as_compound_table(data)
+}
+
+#' @export
+load_expected_adducts_csv <- function (file) {
+  data <- load_csv(file, columns = "adduct")
+  as_ecpected_adducts_table(data)
+}
+
+#' @export
+load_boost_compounds_csv <- function (file) {
+  data <- load_csv(file, columns = c("compound", "mz", "rt"))
+  as_boosted_compounds_table(data)
+}
+
+#' @export
+load_peak_table_hdf <- function(file, intensities = FALSE) {
+  data <- rhdf5::h5read(file, "peaks")
+  as_peak_table(data, intensities = intensities)
+}
+
+#' @export
 save_parquet <- function(data, file) {
   invisible(arrow::write_parquet(data, file))
-}
-
-load_parquet <- function(file, required_columns, optional_columns = NULL) {
-  df <- tryCatch(
-    arrow::read_parquet(file, col_select = dplyr::any_of(union(required_columns, optional_columns))),
-    error = function(e) stop("The file ", toString(file), " is not a valid Parquet file")
-  )
-
-  if (!all(required_columns %in% colnames(df))) {
-    stop("The file ", toString(file), " does not contain any of these required columns: ", toString(required_columns))
-  }
-
-  return(df)
-}
-
-load_csv <- function (file, required_columns, optional_columns = NULL) {
-  df <- readr::read_csv(file)
-  df <- dplyr::select(df, dplyr::any_of(union(required_columns, optional_columns)))
-
-  if (!all(required_columns %in% colnames(df))) {
-    stop("The file ", toString(file), " does not contain any of these required columns: ", toString(required_columns))
-  }
-
-  return(df)
-}
-
-load_peak_table_parquet <- function(file) {
-  load_parquet(file, required_columns = c("peak", "mz", "rt"))
-}
-
-load_adduct_table_parquet <- function(file) {
-  load_parquet(file, required_columns = c("adduct", "charge", "mass", "n_molecules"))
-}
-
-load_compound_table_parquet <- function(file) {
-  compound_table <- load_parquet(
-    file,
-    required_columns = c("monoisotopic_mass", "molecular_formula"),
-    optional_columns = c("compound", "recetox_cid")
-  )
-
-  if (!any(c("compound", "recetox_cid") %in% colnames(compound_table))) {
-    stop("The supplied compound table needs to contain either compound or recetox_cid column")
-  }
-
-  if ("recetox_cid" %in% colnames(compound_table)) {
-    compound_table <- dplyr::mutate(compound_table, compound = recetox_cid)
-  }
-
-  return(compound_table)
-}
-
-load_peak_table_hdf <- function(file) {
-  peak_table <- rhdf5::h5read(file, "peaks")
-  peak_table <- dplyr::select(
-    peak_table,
-    dplyr::any_of("peak"),
-    dplyr::all_of(c("mz", "rt")),
-    dplyr::starts_with("intensity")
-  )
-
-  if (!"peak" %in% colnames(peak_table)) {
-    peak_table <- dplyr::mutate(peak_table, peak = lexicographic_rank(mz, rt))
-  }
-
-  return(peak_table)
-}
-
-load_expected_adducts_csv <- function (file) {
-  load_csv(file, required_columns = "adduct")
-}
-
-load_boost_compounds_csv <- function (file) {
-  load_csv(file, required_columns = "compound", optional_columns = c("mz", "rt"))
 }
