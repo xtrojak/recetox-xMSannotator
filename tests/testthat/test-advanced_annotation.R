271f5ff6fc962333ca67715e2c7bdae3dd0e569b
hechth <helge.hecht@gmx.de>
2021-07-08 15:04:16 +0200

    ¨Reformatted document and added specific adducts

--- tests/testthat/test-advanced_annotation.R
+++ tests/testthat/test-advanced_annotation.R
@@ -2,19 +2,27 @@ test_that("advanced annotation works", {
   tmpdir <- tempdir()
   load("testdata/sample_peaks.rda")
   load("testdata/expected_stage_1.rda")
-  #load("testdata/expected_stage_2.rda")
+
+  queryadductlist <- c(
+    "M+H", "M+2H", "M+H+NH4", "M+ACN+2H",
+    "M+2ACN+2H", "M+NH4", "M+Na", "M+ACN+H",
+    "M+ACN+Na", "M+2ACN+H", "2M+H", "2M+Na",
+    "2M+ACN+H", "M+2Na-H", "M+H-H2O", "M+H-2H2O"
+  )
 
   annotation <- multilevelannotation(
     peaks,
     num_nodes = 8,
     outloc = tmpdir,
     allsteps = FALSE,
-    db_name = "HMDB"
+    db_name = "HMDB",
+    queryadductlist = queryadductlist,
+    num_sets = 300,
   )
 
   actual_stage_1 <- read.csv("Stage1.csv")
-  #actual_stage_2 <- read.csv("Stage2.csv")
 
+  actual_stage_1 <- dplyr::arrange(actual_stage_1, Module_RTclust, mz, time)
+  expected_stage_1 <- dplyr::arrange(expected_stage_1, Module_RTclust, mz, time)
   expect_equal(actual_stage_1, expected_stage_1)
-  #expect_equal(actual_stage_2, expected_stage_2)
 })
