971d3002c440ec599d8188b747e4595ef7253a0d
hechth <helge.hecht@gmx.de>
2021-07-08 16:48:09 +0200

    Finished test for advanced annotation stage 1

--- tests/testthat/test-advanced_annotation.R
+++ tests/testthat/test-advanced_annotation.R
@@ -1,6 +1,8 @@
-test_that("advanced annotation works", {
+test_that("advanced annotation Stage1 works", {
   tmpdir <- tempdir()
   load("testdata/sample_peaks.rda")
+  peaks <- unique(peaks)
+
   load("testdata/expected_stage_1.rda")
 
   queryadductlist <- c(
@@ -9,6 +11,7 @@ test_that("advanced annotation works", {
     "M+ACN+Na", "M+2ACN+H", "2M+H", "2M+Na",
     "2M+ACN+H", "M+2Na-H", "M+H-H2O", "M+H-2H2O"
   )
+  comparison_columns <- c("mz", "time", "rep1", "rep2", "rep3")
 
   annotation <- multilevelannotation(
     peaks,
@@ -16,13 +19,20 @@ test_that("advanced annotation works", {
     outloc = tmpdir,
     allsteps = FALSE,
     db_name = "HMDB",
-    queryadductlist = queryadductlist,
-    num_sets = 300,
+    queryadductlist = queryadductlist
   )
 
   actual_stage_1 <- read.csv("Stage1.csv")
 
-  actual_stage_1 <- dplyr::arrange(actual_stage_1, Module_RTclust, mz, time)
-  expected_stage_1 <- dplyr::arrange(expected_stage_1, Module_RTclust, mz, time)
+  actual_stage_1 <- subset(actual_stage_1, select = -Module_RTclust)
+  expected_stage_1 <- subset(expected_stage_1, select = -Module_RTclust)
+
+  actual_stage_1 <- dplyr::arrange(
+    actual_stage_1, mz, time, rep1, rep2, rep3
+  )
+  expected_stage_1 <- dplyr::arrange(
+    expected_stage_1, mz, time, rep1, rep2, rep3
+  )
+
   expect_equal(actual_stage_1, expected_stage_1)
 })
