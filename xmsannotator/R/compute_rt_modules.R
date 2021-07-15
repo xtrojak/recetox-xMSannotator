f2924e7c8a4dc9e366925ee18b4440105c80cf77
hechth <helge.hecht@recetox.muni.cz>
2021-07-15 15:53:52 +0200

    Finalized computation and test

--- xmsannotator/R/compute_rt_modules.R
+++ xmsannotator/R/compute_rt_modules.R
@@ -1,12 +1,16 @@
 #' @import dplyr
 #' @export
-compute_rt_modules <- function(peak_table, peak_modules) {
+compute_rt_modules <- function(peak_table, peak_modules, bandwidth = 1) {
     peaks <- inner_join(peak_table, peak_modules, by = "peak")
     modules <- unique(peak_modules$module)
     rt_cluster <- {}
     for (id in modules) {
         subdata <- peaks %>% slice(filter(peak_modules, peak_modules$module == id)$peak)
-        subdata$rt_cluster <- meanShiftR::meanShift(subdata$rt, iterations = 100)$assignment
+        subdata$rt_cluster <- meanShiftR::meanShift(
+            subdata$rt,
+            iterations = 100,
+            bandwidth = bandwidth
+        )$assignment
         rt_cluster <- bind_rows(rt_cluster, subdata %>% select(peak, rt_cluster))
     }
     return(rt_cluster)
--- xmsannotator/tests/testthat/test_compute_rt_modules.R
+++ xmsannotator/tests/testthat/test_compute_rt_modules.R
@@ -1,42 +1,33 @@
-library(xmsannotator)
-library(dplyr)
-library(tibble)
+test_that("RT based clustering works", {
 
-compute_rt_modules <- function(peak_table, peak_modules) {
-    peaks <- inner_join(peak_table, peak_modules, by = "peak")
-    modules <- unique(peak_modules$module)
-    rt_cluster <- {}
-    for (id in modules) {
-        subdata <- peaks %>% slice(filter(peak_modules, peak_modules$module == id)$peak)
-        subdata$rt_cluster <- meanShiftR::meanShift(subdata$rt, iterations = 100)$assignment
-        rt_cluster <- bind_rows(rt_cluster, subdata %>% select(peak, rt_cluster))
-    }
-    return(rt_cluster)
-}
+    load("test-data/sample_peaks.rda")
+    peaks$peak <- as.integer(rownames(peaks))
+    names(peaks) <- c("mz", "rt", "intensity_rep1", "intensity_rep2", "intensity_rep3", "peak")
+    peak_table <- peaks
+    peaks <- NA
 
-load("/home/hechth/dev/git/hechth/recetox-xMSannotator/xmsannotator/tests/testthat/test-data/sample_peaks.rda")
-peaks$peak <- as.integer(rownames(peaks))
-names(peaks) <- c("mz", "rt", "intensity_rep1", "intensity_rep2", "intensity_rep3", "peak")
-peak_table <- peaks
-peaks <- NA
+    peak_intensity_matrix <- t(select(peak_table, starts_with("intensity")))
+    peak_intensity_matrix <- magrittr::set_colnames(peak_intensity_matrix, peak_table$peak)
+    peak_correlation_matrix <- WGCNA::cor(peak_intensity_matrix, use = "p", method = "p")
 
-peak_intensity_matrix <- t(select(peak_table, starts_with("intensity")))
-peak_intensity_matrix <- magrittr::set_colnames(peak_intensity_matrix, peak_table$peak)
-peak_correlation_matrix <- WGCNA::cor(peak_intensity_matrix, use = "p", method = "p")
+    correlation_threshold <- 0.7
+    deep_split <- 2
+    min_cluster_size <- 10
+    network_type <- "unsigned"
 
-correlation_threshold = 0.7
-deep_split = 2
-min_cluster_size = 10
-network_type = "unsigned"
+    peak_modules <- compute_peak_modules(
+        peak_intensity_matrix = peak_intensity_matrix,
+        peak_correlation_matrix = peak_correlation_matrix,
+        correlation_threshold = correlation_threshold,
+        deep_split = deep_split,
+        min_cluster_size = min_cluster_size,
+        network_type = network_type
+    )
 
-peak_modules <- compute_peak_modules(
-  peak_intensity_matrix = peak_intensity_matrix,
-  peak_correlation_matrix = peak_correlation_matrix,
-  correlation_threshold = correlation_threshold,
-  deep_split = deep_split,
-  min_cluster_size = min_cluster_size,
-  network_type = network_type
-)
+    rt_modules <- compute_rt_modules(peak_table, peak_modules, bandwidth = 10)
 
-stage1 <- compute_rt_modules(peak_table, peak_modules)
-names(stage1)
+    peaks <- left_join(peak_table, peak_modules, by = "peak")
+    stage1 <- left_join(peaks, rt_modules, by = "peak")
+    stage1$Module_RTclust <- stringr::str_c(stage1$module, stage1$rt_cluster, sep = "_")
+    write.csv(stage1, file = "stage1_rcx_final.csv")
+})
