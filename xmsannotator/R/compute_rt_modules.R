a94249127f00b0b921241b12d5480128c14f1969
hechth <helge.hecht@recetox.muni.cz>
2021-07-15 15:21:21 +0200

    Added rt module clustering and basic test

---
+++ xmsannotator/R/compute_rt_modules.R
@@ -0,0 +1,13 @@
+#' @import dplyr
+#' @export
+compute_rt_modules <- function(peak_table, peak_modules) {
+    peaks <- inner_join(peak_table, peak_modules, by = "peak")
+    modules <- unique(peak_modules$module)
+    rt_cluster <- {}
+    for (id in modules) {
+        subdata <- peaks %>% slice(filter(peak_modules, peak_modules$module == id)$peak)
+        subdata$rt_cluster <- meanShiftR::meanShift(subdata$rt, iterations = 100)$assignment
+        rt_cluster <- bind_rows(rt_cluster, subdata %>% select(peak, rt_cluster))
+    }
+    return(rt_cluster)
+}
