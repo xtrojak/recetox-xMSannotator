80961b633f4e94c2333df978537501232918940b
hechth <helge.hecht@recetox.muni.cz>
2021-07-23 14:34:18 +0200

    Cleaned up tests

--- xmsannotator/tests/testthat/test_compute_rt_modules.R
+++ xmsannotator/tests/testthat/test_compute_rt_modules.R
@@ -1,14 +1,16 @@
-test_that("RT based clustering works", {
+test_that("Integration test: RT based clustering works", {
+    peaks <- readRDS("test-data/qc_solvent.rda")
+    peaks <- dplyr::rename(peaks, peak = feature)
+    peaks <- dplyr::rename_with(
+        peaks,
+        ~ paste0("intensity_", .x),
+        starts_with("Tribrid")
+    )
+    peaks$peak <- as.integer(peaks$peak)
 
-    load("test-data/sample_peaks.rda")
-    peaks$peak <- as.integer(rownames(peaks))
-    names(peaks) <- c("mz", "rt", "intensity_rep1", "intensity_rep2", "intensity_rep3", "peak")
-    peak_table <- peaks
-    peaks <- NA
-
-    peak_intensity_matrix <- t(select(peak_table, starts_with("intensity")))
-    peak_intensity_matrix <- magrittr::set_colnames(peak_intensity_matrix, peak_table$peak)
-    peak_correlation_matrix <- WGCNA::cor(peak_intensity_matrix, use = "p", method = "p")
+    intensity_matrix <- t(select(peaks, starts_with("intensity")))
+    intensity_matrix <- magrittr::set_colnames(intensity_matrix, peaks$peak)
+    correlation_matrix <- WGCNA::cor(intensity_matrix, use = "p", method = "p")
 
     correlation_threshold <- 0.7
     deep_split <- 2
@@ -16,18 +18,15 @@ test_that("RT based clustering works", {
     network_type <- "unsigned"
 
     peak_modules <- compute_peak_modules(
-        peak_intensity_matrix = peak_intensity_matrix,
-        peak_correlation_matrix = peak_correlation_matrix,
+        peak_intensity_matrix = intensity_matrix,
+        peak_correlation_matrix = correlation_matrix,
         correlation_threshold = correlation_threshold,
         deep_split = deep_split,
         min_cluster_size = min_cluster_size,
         network_type = network_type
     )
 
-    rt_modules <- compute_rt_modules(peak_table, peak_modules, bandwidth = 10)
-
-    peaks <- left_join(peak_table, peak_modules, by = "peak")
-    stage1 <- left_join(peaks, rt_modules, by = "peak")
-    stage1$Module_RTclust <- stringr::str_c(stage1$module, stage1$rt_cluster, sep = "_")
-    write.csv(stage1, file = "stage1_rcx_final.csv")
+    peaks <- inner_join(peaks, peak_modules, by = "peak")
+    rt_modules <- compute_rt_modules(peaks)
+    expect_equal(nrow(peaks), nrow(rt_modules))
 })
