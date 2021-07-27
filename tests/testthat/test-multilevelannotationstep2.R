6fbec4bd38085b0728111aab4537493617cc38ce
hechth <helge.hecht@gmx.de>
2021-07-27 11:51:00 +0200

    Added test data and finished first simple test

--- tests/testthat/test-multilevelannotationstep2.R
+++ tests/testthat/test-multilevelannotationstep2.R
@@ -1,4 +1,18 @@
 test_that("multilevelannotationstep2", {
+    # Arrange
     setwd("testdata/multilevelannotationstep2")
+    load(file = "chem_score1.rda")
+    expected <- curchemscoremat
+    curchemscoremat <- NA
+
+    # Act
     multilevelannotationstep2(".", 1)
+    load(file = "chem_score1.Rda")
+
+    # Assert
+    expect_equal(curchemscoremat, expected)
+
+    # Annihilate
+    setwd("../../..")
+    unlink("stage2", recursive = TRUE)
 })
