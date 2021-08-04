e0d076150b24dd5ab2f751ce171b3a0714713069
maximskorik <maximskoryk@gmail.com>
2021-08-04 16:46:23 +0200

    Integrate isotope matching into score computation and adjust the test accordingly

--- xmsannotator/R/compute_scores.R
+++ xmsannotator/R/compute_scores.R
@@ -1,17 +1,26 @@
+# Get abundance ratio of the second most abundant isotopologue
+#' @import Rdisop
+compute_abundance_ratio <- function(formula) {
+  molecule <- getMolecule(formula)
+  isotope <- getIsotope(molecule, 2)
+  abundance_ratio <- isotope[2]
+}
+
 #' @import dplyr
 #' @importFrom rlang .data
-compute_isotopes <- function(peaks, query, rt_tolerance, mass_defect_tolerance = 0, abundance_ratio, max_isp) {
+compute_isotopes <- function(..., peaks, rt_tolerance, mass_defect_tolerance = 0, max_isp) {
+  query <- tibble(...)
   isotopes <- mutate(
     peaks,
     isotopic_deviation = round(mz - query$expected_mass),
     compound = query$compound,
     adduct = query$adduct,
-    formula = query$formula
+    molecular_formula = query$molecular_formula
   )
   isotopes <- filter(
     isotopes,
     cluster == query$cluster,
-    avg_intensity / query$avg_intensity <= abundance_ratio,
+    mean_intensity / query$mean_intensity <= query$abundance_ratio,
     near(rt, query$rt, rt_tolerance),
     near(mass_defect, query$mass_defect, mass_defect_tolerance),
     between(abs(isotopic_deviation), 1, max_isp)
@@ -20,15 +29,20 @@ compute_isotopes <- function(peaks, query, rt_tolerance, mass_defect_tolerance =
 
 #' @import dplyr
 #' @importFrom rlang .data
-compute_scores <- function(annotation, adduct_weights, time_tolerance) {
+compute_scores <- function(annotation, adduct_weights, mass_defect_tolerance, max_isp, peaks, rt_tolerance) {
   annotation <- filter(annotation, forms_valid_adduct_pair(.data$molecular_formula, .data$adduct))
-  annotation <- left_join(annotation, adduct_weights, by = "adduct")
 
   isotopes <- semi_join(annotation, adduct_weights, by = "adduct")
-  isotopes <- purrr::pmap_dfr(isotopes, ~tibble())
+  isotopes <- mutate(isotopes, abundance_ratio = map_dbl(molecular_formula, compute_abundance_ratio))
+  isotopes <- purrr::pmap_dfr(isotopes,
+                              ~compute_isotopes(..., peaks = peaks,
+                                                rt_tolerance = rt_tolerance,
+                                                mass_defect_tolerance = mass_defect_tolerance,
+                                                max_isp = max_isp))
   isotopes <- distinct(isotopes)
 
   annotation <- bind_rows(annotation, isotopes)
+  annotation <- left_join(annotation, adduct_weights, by = "adduct")
   annotation <- group_by(annotation, .data$compound)
   annotation <- mutate(annotation, score = 10^max(-Inf, weight, na.rm = TRUE))
   annotation <- ungroup(annotation)
--- xmsannotator/tests/testthat/test_isotope_matching.R
+++ xmsannotator/tests/testthat/test_isotope_matching.R
@@ -3,9 +3,12 @@ test_that("isotope_matching", {
   query <- readRDS("testdata/query.Rda")
   expected <- readRDS("testdata/isotopes.Rda")
 
-  actual <- compute_isotopes(peaks = peaks, query = query,
-                             rt_tolerance = 1, mass_defect_tolerance = 0.1,
-                             abundance_ratio = 0.2, max_isp = 5)
+
+  actual <- compute_isotopes(peaks = peaks,
+                             rt_tolerance = 1,
+                             mass_defect_tolerance = 0.1,
+                             max_isp = 5,
+                             query)
 
   expect_equal(actual, expected, ignore_attr = TRUE)
 })
