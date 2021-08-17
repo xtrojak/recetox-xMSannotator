38ad17301044ce11e121e90eabbc85b4fea61ff4
Matej Trojak <trojak@mail.muni.cz>
2021-08-17 17:39:40 +0200

    Added adduct table to real data test case

--- xmsannotator/tests/testthat/test_simple_annotation.R
+++ xmsannotator/tests/testthat/test_simple_annotation.R
@@ -13,9 +13,10 @@ test_that("simple_annotation functionality on real data", {
   skip("Currently excluded due to long computational time.")
   peaks <- arrow::read_parquet("test-data/simple_annotation_real/peaks.parquet")
   DB <- arrow::read_parquet("test-data/simple_annotation_real/compounds.parquet")
+  adducts <- arrow::read_parquet("test-data/simple_annotation_real/adducts.parquet")
   expected <- arrow::read_parquet("test-data/simple_annotation_real/expected.parquet")
 
-  result <- simple_annotation(peaks, DB, mass_tolerance=10e-6)
+  result <- simple_annotation(peaks, DB, adduct_table=adducts, mass_tolerance=10e-6)
   
   names(result) <- c("Peak", "Adduct", "compound", "expected_mass", "mz", "time", "Name", "MonoisotopicMass", "Formula", "MatchCategory")
   
