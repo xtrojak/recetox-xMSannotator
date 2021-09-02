6b66298aa9c4f963c613dcd45539b13913c93ebf
maximskorik <maximskoryk@gmail.com>
2021-09-02 13:26:46 +0200

    Add proper punctuation in documentation

--- xmsannotator/R/compute_scores.R
+++ xmsannotator/R/compute_scores.R
@@ -1,8 +1,8 @@
-#' Assign the abundance ratio of the second most abundant isotope/isotopologue for each molecule in a table
+#' Assign the abundance ratio of the second most abundant isotope/isotopologue for each molecule in a table.
 #'
-#' @param isotopes A table with annotated peaks
+#' @param isotopes A table with annotated peaks.
 #'
-#' @return The input table with an extra column containing the normalized abundance ratio of the second most abundant isotope/isotopologue
+#' @return The input table with an extra column containing the normalized abundance ratio of the second most abundant isotope/isotopologue.
 #'
 #' @import dplyr
 #' @import purrr
@@ -12,10 +12,10 @@ assign_isotope_abundances <- function(isotopes) {
   isotopes <- inner_join(isotopes, molecules, by = "molecular_formula")
 }
 
-#' Compute isotopic pattern of a given molecule
+#' Compute isotopic pattern of a given molecule.
 #'
-#' @param formula A string containing molecular or empirical formula of a compound
-#' @param minAbund A cutoff abundance ratio: only isotopes with abundance ratio above this value are returned
+#' @param formula A string containing molecular or empirical formula of a compound.
+#' @param minAbund A cutoff abundance ratio: only isotopes with abundance ratio above this value are returned.
 #'
 #' @return A dataframe containing isotopic pattern of a given molecule. The pattern is represented by three columns:
 #' mass, mass number difference, and abundance. Abundance of the isotopes is normalized with the most abundant isotope
@@ -34,18 +34,18 @@ compute_isotopic_pattern <- function(formula, minAbund = 0.001) {
 
 #' Match isotopes from peak table to annotated peaks based on rt cluster, rt difference, peak intensity, and mass defect.
 #'
-#' @param ... A single isotope passed as a list of columns. See [purrr::map()] for more details
+#' @param ... A single isotope passed as a list of columns. See [purrr::map()] for more details.
 #' @param intensity_deviation_tolerance A numeric threshold by which an intensity ratio of two isotopic peaks may differ
-#'  from their actual abundance ratio
+#'  from their actual abundance ratio.
 #' @param peaks A peak table containing a peak identifier (unique number), mean intensity, module, and rt cluster
-#'  of each identified peak
-#' @param mass_defect_tolerance A number. Maximum difference in mass defect between two peaks of the same compound
-#' @param max_isp Maximal number of unique isotopes of a single compound
-#' @param rt_tolerance A number. Maximum rt difference for two peaks of the same substance
+#'  of each identified peak.
+#' @param mass_defect_tolerance A number. Maximum difference in mass defect between two peaks of the same compound.
+#' @param max_isp Maximal number of unique isotopes of a single compound.
+#' @param rt_tolerance A number. Maximum rt difference for two peaks of the same substance.
 #'
 #' @return A table of identified isotopes with an extra column: `mass_number_difference`.
 #'  The extra column represents a nominal mass (mass number) difference between a given isotope and the most abundant
-#'  isotope of the same molecule
+#'  isotope of the same molecule.
 #'
 #' @import dplyr
 #' @importFrom rlang .data
@@ -75,15 +75,15 @@ compute_isotopes <- function(...,
 
 #' Match prefiltered isotopes by comparing their relative intensity to their relative natural abundance.
 #' The relative values are computed as a ratio of an isotopic peak intensity to the intensity of its monoisotopic peak,
-#' respectively as a ratio of a natural abundance of the isotope to the abundance of the most abundant isotope
+#' respectively as a ratio of a natural abundance of the isotope to the abundance of the most abundant isotope.
 #'
-#' @param query A single annotated peak
-#' @param isotopes Isotope candidates for the annotated molecule. The candidates are identified in [compute_isotopes]
-#' @param pattern Isotopic pattern of a given molecule as produced by [compute_isotopic_pattern]
+#' @param query A single annotated peak.
+#' @param isotopes Isotope candidates for the annotated molecule. The candidates are identified in [compute_isotopes].
+#' @param pattern Isotopic pattern of a given molecule as produced by [compute_isotopic_pattern].
 #' @param intensity_deviation_tolerance A numeric threshold by which an intensity ratio of two isotopic peaks may differ
-#'  from their actual abundance ratio
+#'  from their actual abundance ratio.
 #'
-#' @return A table of matched isotopes
+#' @return A table of matched isotopes.
 #'
 #' @import dplyr
 match_isotopes_by_intensity <- function(query,
@@ -100,17 +100,17 @@ match_isotopes_by_intensity <- function(query,
   isotopes <- select(isotopes, -c(abund, relative_intensity))
 }
 
-#' Compute a confidence score for each annotation
+#' Compute a confidence score for each annotation.
 #'
-#' @param annotation A table with annotated peaks
-#' @param adduct_weights A weight-by-adduct table
+#' @param annotation A table with annotated peaks.
+#' @param adduct_weights A weight-by-adduct table.
 #' @param intensity_deviation_tolerance A numeric threshold by which an intensity ratio of two isotopic peaks may differ
-#'  from their actual abundance ratio
-#' @param mass_defect_tolerance A number. Maximum difference in mass defect between two peaks of the same compound
-#' @param max_isp Maximal number of unique isotopes of a single compound
+#'  from their actual abundance ratio.
+#' @param mass_defect_tolerance A number. Maximum difference in mass defect between two peaks of the same compound.
+#' @param max_isp Maximal number of unique isotopes of a single compound.
 #' @param peaks A peak table containing a peak identifier (unique number), mean intensity, module, and rt cluster
-#'  of each identified peak
-#' @param rt_tolerance A number. Maximum rt difference for two peaks of the same substance
+#'  of each identified peak.
+#' @param rt_tolerance A number. Maximum rt difference for two peaks of the same substance.
 #'
 #' @import dplyr
 #' @importFrom rlang .data
