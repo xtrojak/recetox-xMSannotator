5db7ade1259e4178eb0def94e292eb9a34018792
hechth <helge.hecht@recetox.muni.cz>
2021-09-09 18:25:02 +0200

    Made test parameterized

--- tests/testthat/test-multilevelannotationstep5.R
+++ tests/testthat/test-multilevelannotationstep5.R
@@ -1,12 +1,20 @@
-test_that("multilevelannotationstep5", {
-    skip("Currently excluded!")
-    load("testdata/multilevelannotationstep5/tempobjects.Rda")
+patrick::with_parameters_test_that("multilevelannotationstep5", {
     load("testdata/adduct_weights.rda")
 
-    outloc <- tempdir()
-    file.copy("testdata/multilevelannotationstep5/Stage4.csv", file.path(outloc, "Stage4.csv"))
+    testdata_dir <- file.path("testdata", subfolder)
+    load(file.path(testdata_dir, "tempobjects.Rda"))
+    
+    testthat_wd <- getwd()
 
-    expected <- read.csv("testdata/multilevelannotationstep5/Stage5.csv")
+    outloc <- file.path(tempdir(), subfolder, "multilevelannotationstep5")
+    dir.create(outloc, recursive = TRUE)
+    file.copy(
+        file.path(testdata_dir,"Stage4.csv"),
+        file.path(outloc, "Stage4.csv")
+    )
+    expected <- read.csv(file.path(testdata_dir, "Stage5.csv"))
+
+    
     multilevelannotationstep5(
         outloc = outloc,
         max.rt.diff = max_diff_rt,
@@ -22,4 +30,10 @@ test_that("multilevelannotationstep5", {
     actual <- read.csv(file.path(outloc, "Stage5.csv"))
 
     expect_equal(actual, expected)
-})
+    setwd(testthat_wd)
+}, patrick::cases(
+    qc_solvent = list(subfolder = "qc_solvent"),
+    qc_matrix = list(subfolder = "qc_matrix"),
+    batch1_neg = list(subfolder = "batch1_neg"),
+    sourceforge = list(subfolder = "sourceforge")
+))
