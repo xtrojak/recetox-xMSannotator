3ee181b41df29873e11c0cec1384548d7ceb79d8
maximskorik <maximskoryk@gmail.com>
2021-09-14 17:19:33 +0200

    Reformat tests & clear test data

--- xmsannotator/tests/testthat/test_score_computation.R
+++ xmsannotator/tests/testthat/test_score_computation.R
@@ -1,16 +1,28 @@
-test_that("isotope_matching", {
-  peaks <- readRDS("test-data/score_computation/peaks.Rda")
-  query <- readRDS("test-data/score_computation/query.Rda")
-  expected <- readRDS("test-data/score_computation/isotopes.Rda")
-
+patrick::with_parameters_test_that("Chemscore computing: isotope matching", {
+  peaks <- readRDS("test-data/score_computation/peaks.rds")
   actual <- compute_isotopes(peaks = peaks,
-                             rt_tolerance = 1,
+                             intensity_deviation_tolerance = 0.25,
+                             rt_tolerance = 10,
                              mass_defect_tolerance = 0.1,
                              max_isp = 5,
                              query)
-
   expect_equal(actual, expected, ignore_attr = TRUE)
-})
+},
+  patrick::cases(
+    isoproturon = list(
+      query = readRDS("test-data/score_computation/isoproturon.rds"),
+      expected = readRDS("test-data/score_computation/isoproturon_isp.rds")
+    ),
+    desisopropylatrazine = list(
+      query = readRDS("test-data/score_computation/desisopropylatrazine.rds"),
+      expected = readRDS("test-data/score_computation/desisopropylatrazine_isp.rds")
+    ),
+    acetochlor = list(
+      query = readRDS("test-data/score_computation/acetochlor.rds"),
+      expected = readRDS("test-data/score_computation/acetochlor_isp.rds")
+    )
+  )
+)
 
 test_that("Chemscore computing: isotope matching by intensity.", {
   query <- readRDS("test-data/score_computation/query.Rda")
@@ -25,19 +37,20 @@ test_that("Chemscore computing: isotope matching by intensity.", {
   expect_equal(actual, expected)
 })
 
-patrick::with_parameters_test_that("abundance_ratio_computing", {
+patrick::with_parameters_test_that("Chemscore computing: theoretical isotopic pattern", {
   actual <- compute_isotopic_pattern(formula, minAbund = 0.001)
   expect_equal(actual, expected)
 },
-  patrick::cases(carbon = list(
+  patrick::cases(
+    carbon = list(
         formula = "C",
         expected = readRDS("test-data/score_computation/C_pattern.rda")
         ),
-        chlorophorm = list(
+    chlorophorm = list(
         formula = "CHCl3",
         expected = readRDS("test-data/score_computation/CHCl3_pattern.rda")
         ),
-        chlorophyll = list(
+    chlorophyll = list(
         formula = "C55H72O5N4Mg",
         expected = readRDS("test-data/score_computation/CHLA_pattern.rda")
         )
