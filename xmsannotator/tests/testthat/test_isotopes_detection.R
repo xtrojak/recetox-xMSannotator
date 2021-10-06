5de9d7ba74b8f54f0e5cf795773a6d1a5dd2315c
maximskorik <maximskoryk@gmail.com>
2021-10-06 14:36:54 +0200

    Add a general test-case

--- xmsannotator/tests/testthat/test_isotopes_detection.R
+++ xmsannotator/tests/testthat/test_isotopes_detection.R
@@ -1,4 +1,20 @@
-patrick::with_parameters_test_that("Isotopes-computing: isotopes detection", {
+test_that("Isotopes-computing: isotopes annotation.", {
+  peak_table <- readRDS("test-data/isotopes_detection/peaks.rds")
+  annotation <- readRDS("test-data/isotopes_detection/annotation.rds")
+  adduct_weights <- tibble(adduct = c("M+H", "M+NH4"), weight = c(5, 5))
+
+  actual <- compute_isotopes(annotation = annotation,
+                             adduct_weights = adduct_weights,
+                             intensity_deviation_tolerance = 0.25,
+                             peak_table = peak_table,
+                             rt_tolerance = 10,
+                             mass_defect_tolerance = 0.1
+  )
+  expected <- readRDS("test-data/isotopes_detection/annotation_with_isp.rds")
+  expect_equal(actual, expected)
+})
+
+patrick::with_parameters_test_that("Isotopes-computing: isotopes detection (single annotation).", {
   peaks <- readRDS("test-data/isotopes_detection/peaks.rds")
   actual <- detect_isotopic_peaks(peaks = peaks,
                              intensity_deviation_tolerance = 0.25,
