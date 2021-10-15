4ab723e90cb7df6d85e0cc8c07c0bc48edd1353d
maximskorik <maximskoryk@gmail.com>
2021-10-15 16:43:57 +0200

    Add missing columns to table constructor

--- xmsannotator/R/integration_utils.R
+++ xmsannotator/R/integration_utils.R
@@ -11,6 +11,7 @@ construct_adduct_formula <- function(formula, mass_number_difference) {
 #' @importFrom rlang .data
 #' @export
 reformat_annotation_table <- function(annotation, supplementary_data) {
+  annotation <- left_join(annotation, supplementary_data, by = c("peak", "mz", "rt"))
   master_annotation <- tibble(.rows = nrow(annotation))
 
   master_annotation <- master_annotation %>%
@@ -18,12 +19,15 @@ reformat_annotation_table <- function(annotation, supplementary_data) {
       Module_RTclust = paste(annotation$module, annotation$rt_cluster, sep = "_"),
       mz = annotation$mz,
       time = annotation$rt,
+      MatchCategory = if_else(isTRUE(annotation$multiple_match), "Multiple", "Unique"),
+      theoretical.mz = annotation$expected_mass,
+      Name = annotation$name,
       Formula = annotation$molecular_formula,
       Adduct = if_else(annotation$mass_number_difference == 0, annotation$adduct, construct_adduct_formula(annotation$adduct, annotation$mass_number_difference)),
       time.y = annotation$rt,
       ISgroup = character(nrow(annotation)), # dummy column due to high complexity of reproducing the values
       mean_int_vec = annotation$mean_intensity,
-      MD = as.numeric(paste(0, annotation$mass_defect, sep = "."))
+      MD = as.numeric(sprintf("0.%1.0f", annotation$mass_defect))
     )
 
   return(master_annotation)
