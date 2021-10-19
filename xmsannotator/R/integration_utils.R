0cfa2adf00abe3a33a3132bd39fe06991e4ca519
maximskorik <maximskoryk@gmail.com>
2021-10-19 13:26:28 +0200

    Remove wrong column from join; adjust `master` table construction; modify test-data

--- xmsannotator/R/integration_utils.R
+++ xmsannotator/R/integration_utils.R
@@ -11,7 +11,7 @@ construct_adduct_formula <- function(formula, mass_number_difference) {
 #' @importFrom rlang .data
 #' @export
 reformat_annotation_table <- function(annotation, supplementary_data) {
-  annotation <- left_join(annotation, supplementary_data, by = c("peak", "mz", "rt"))
+  annotation <- left_join(annotation, supplementary_data, by = c("mz", "rt"))
   master_annotation <- tibble(.rows = nrow(annotation))
 
   master_annotation <- master_annotation %>%
@@ -19,16 +19,25 @@ reformat_annotation_table <- function(annotation, supplementary_data) {
       Module_RTclust = paste(annotation$module, annotation$rt_cluster, sep = "_"),
       mz = annotation$mz,
       time = annotation$rt,
-      MatchCategory = if_else(isTRUE(annotation$multiple_match), "Multiple", "Unique"),
+      MatchCategory = case_when(is.na(annotation$multiple_match) ~ "-",
+                                isTRUE(annotation$multiple_match) ~ "Multiple",
+                                !isTRUE(annotation$multiple_match) ~ "Unique"),
       theoretical.mz = annotation$expected_mass,
-      Name = annotation$name,
+      chemical_ID = paste("Formula", annotation$compound, sep = "_"),
+      Name = annotation$Name,
       Formula = annotation$molecular_formula,
+      MonoisotopicMass = annotation$monoisotopic_mass,
       Adduct = if_else(annotation$mass_number_difference == 0, annotation$adduct, construct_adduct_formula(annotation$adduct, annotation$mass_number_difference)),
+      ISgroup = "-", # dummy column due to high complexity of reproducing the values
       time.y = annotation$rt,
-      ISgroup = character(nrow(annotation)), # dummy column due to high complexity of reproducing the values
       mean_int_vec = annotation$mean_intensity,
       MD = as.numeric(sprintf("0.%1.0f", annotation$mass_defect))
     )
+
+  master_annotation <- master_annotation %>%
+    group_by(chemical_ID) %>%
+    mutate(Name = filter(master_annotation, !is.na(Name))$Name[1]) %>%
+    ungroup()
 
   return(master_annotation)
 }
