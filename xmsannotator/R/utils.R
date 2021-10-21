0e833104460970066e13cfda0e30ba3e6a2d57d3
maximskorik <maximskoryk@gmail.com>
2021-10-21 15:42:48 +0200

    Apply `styler`

--- xmsannotator/R/utils.R
+++ xmsannotator/R/utils.R
@@ -1,19 +1,19 @@
 lexicographic_rank <- function(...) {
   .o <- order(...)
-  .x <- cbind(...)[.o,]
+  .x <- cbind(...)[.o, ]
   cumsum(!duplicated(.x))[order(.o)]
 }
 
 #' @import dplyr
 as_peak_table <- function(data, intensities = FALSE) {
-  optional <- rlang::quo(any_of('peak'))
-  required <- rlang::quo(any_of(c('mz', 'rt')))
+  optional <- rlang::quo(any_of("peak"))
+  required <- rlang::quo(any_of(c("mz", "rt")))
 
   if (!intensities) {
     data <- select(data, !!optional, !!required)
   }
 
-  if (!is.element('peak', colnames(data))) {
+  if (!is.element("peak", colnames(data))) {
     data$peak <- lexicographic_rank(data$mz, data$rt)
   }
 
@@ -28,7 +28,7 @@ as_peak_table <- function(data, intensities = FALSE) {
 
 #' @import dplyr
 as_adduct_table <- function(data) {
-  data <- select(data, 'adduct', 'charge', 'factor', 'mass')
+  data <- select(data, "adduct", "charge", "factor", "mass")
 
   stopifnot(anyDuplicated(data$adduct) == 0)
   stopifnot(is.integer(data$charge))
@@ -40,15 +40,15 @@ as_adduct_table <- function(data) {
 
 #' @import dplyr
 as_compound_table <- function(data) {
-  optional <- c('recetox_cid', 'Name')
-  required <- c('monoisotopic_mass', 'molecular_formula', 'compound')
+  optional <- c("recetox_cid", "Name")
+  required <- c("monoisotopic_mass", "molecular_formula", "compound")
   data <- select(data, any_of(optional), all_of(required))
 
-  if ('recetox_cid' %in% colnames(data)) {
+  if ("recetox_cid" %in% colnames(data)) {
     data <- mutate(data, compound = .data$recetox_cid)
   }
 
-  stopifnot('compound' %in% colnames(data))
+  stopifnot("compound" %in% colnames(data))
   stopifnot(anyDuplicated(data$compound) == 0)
   stopifnot(is.numeric(data$compound))
   stopifnot(is.numeric(data$monoisotopic_mass))
@@ -58,14 +58,14 @@ as_compound_table <- function(data) {
 }
 
 #' @import dplyr
-as_expected_adducts_table <- function (data) {
-  data <- select(data, 'adduct')
+as_expected_adducts_table <- function(data) {
+  data <- select(data, "adduct")
   return(data)
 }
 
 #' @import dplyr
-as_boosted_compounds_table <- function (data) {
-  data <- select(data, all_of('compound'), any_of(c('mz', 'rt')))
+as_boosted_compounds_table <- function(data) {
+  data <- select(data, all_of("compound"), any_of(c("mz", "rt")))
 
   stopifnot(is.numeric(data$mz))
   stopifnot(is.numeric(data$rt))
@@ -74,14 +74,14 @@ as_boosted_compounds_table <- function (data) {
 }
 
 #' @import dplyr
-load_parquet <- function (file, columns) {
+load_parquet <- function(file, columns) {
   rlang::with_handlers(
     arrow::read_parquet(file, col_select = any_of(columns)),
     error = ~ rlang::abort(paste("The file", toString(file), "seams not to be a valid Parquet file."), parent = .)
   )
 }
 
-load_csv <- function (file, columns) {
+load_csv <- function(file, columns) {
   data <- readr::read_csv(file)
   data <- select(data, any_of(columns))
 }
@@ -105,13 +105,13 @@ load_compound_table_parquet <- function(file) {
 }
 
 #' @export
-load_expected_adducts_csv <- function (file) {
+load_expected_adducts_csv <- function(file) {
   data <- load_csv(file, columns = "adduct")
   as_expected_adducts_table(data)
 }
 
 #' @export
-load_boost_compounds_csv <- function (file) {
+load_boost_compounds_csv <- function(file) {
   data <- load_csv(file, columns = c("compound", "mz", "rt"))
   as_boosted_compounds_table(data)
 }
