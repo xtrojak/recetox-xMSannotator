e4fde37d084c98aa7cc6e8c0acb36c346f1d6f67
maximskorik <maximskoryk@gmail.com>
2021-10-21 15:52:28 +0200

    Add proper order of operations

--- xmsannotator/tests/testthat/test-utils.R
+++ xmsannotator/tests/testthat/test-utils.R
@@ -1,7 +1,7 @@
 test_that("Utils data reading: peak table processing.", {
   peak_table <- readRDS("test-data/utils/peak_table.rds")
-  peak_table <- select(peak_table, c("peak", "mz", "rt"))
   actual <- as_peak_table(peak_table, intensities = FALSE)
+  peak_table <- select(peak_table, c("peak", "mz", "rt"))
 
   expect_equal(actual, peak_table)
 })
